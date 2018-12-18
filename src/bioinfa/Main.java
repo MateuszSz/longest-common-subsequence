1a4c603d01ede48606237ccbec69a87b43d12555
Mateusz Szymczak <mateuszszymczak15@gmail.com>
2018-12-18 18:48:28 +0100

    refactor

--- src/bioinfa/Main.java
+++ src/bioinfa/Main.java
@@ -1,35 +1,78 @@
 package bioinfa;
 
-import bioinfa.algorithm.seqalign.NeedlemanWunsch;
-import bioinfa.algorithm.seqalign.SequenceAlignment;
-import bioinfa.algorithm.seqalign.SmithWaterman;
-import bioinfa.algorithm.sequence.LongestCommonSubsequence;
-import bioinfa.utils.TablePrinter;
+import bioinfa.algorithm.sequence.NeedlemanWunsch;
+import bioinfa.algorithm.sequence.SequenceAlignment;
+import bioinfa.algorithm.sequence.SmithWaterman;
+import bioinfa.utils.CostHandler;
+import bioinfa.utils.CustomFileReader;
+import bioinfa.utils.RnaAcidTranslator;
 
-import javax.sound.midi.Sequence;
 import java.util.Arrays;
 
 public class Main {
+    public static RnaAcidTranslator rnaAcidTranslator = new RnaAcidTranslator(3);
 
     public static void main(String[] args) {
-        String u = "writers", w = "vintner";
 
-        SequenceAlignment sq = new NeedlemanWunsch(u,w);
-        SequenceAlignment sq2 = new SmithWaterman(u,w);
-        LongestCommonSubsequence ls = new LongestCommonSubsequence(u,w);
+        CustomFileReader customFileReader = new CustomFileReader("src/array.txt");
+        CostHandler costHandler = new CostHandler(
+                customFileReader.getSequences(),
+                customFileReader.getCostList()
+        );
+
+        String u = customFileReader.getSequence1();
+        String w = customFileReader.getSequence2();
+
+        SequenceAlignment sq = new NeedlemanWunsch(u, w);
+        SequenceAlignment sq2 = new SmithWaterman(u, w);
+
+        System.out.printf("Dwie sekwencje: [%s] oraz [%s]%n", u, w);
 
         System.out.println("Najlepsze zestawienie globalne:");
-        TablePrinter.printScoreTable(sq);
-        System.out.println(Arrays.toString(sq.getAlignment()));
-        System.out.println(sq.getAlignmentScore());
+        String[] globalAlignments = sq.getAlignments();
+        System.out.println(Arrays.toString(globalAlignments));
+        System.out.println("Dystans: " + costHandler.getAlignmentDistance(globalAlignments));
+        System.out.println("Podobieństwo: " + costHandler.getAlignmentSimilarity(globalAlignments));
 
         System.out.println("Najlepsze zestawienie lokalne:");
-        TablePrinter.printScoreTable(sq2);
-        System.out.println(Arrays.toString(sq2.getAlignment()));
-        System.out.println(sq2.getAlignmentScore());
+        String[] localAlignments = sq2.getAlignments();
+        System.out.println(Arrays.toString(localAlignments));
+        System.out.println("Dystans: " + costHandler.getAlignmentDistance(localAlignments));
+        System.out.println("Podobieństwo: " + costHandler.getAlignmentSimilarity(localAlignments));
+        System.out.println("---------------------------------------------------------");
 
-        System.out.println("podobienstwo:");
-        TablePrinter.printScoreTable(ls);
-        System.out.println(ls.getLongestCommonSubsequence());
+        customFileReader = new CustomFileReader("src/blosum.txt");
+
+        costHandler = new CostHandler(
+                customFileReader.getSequences(),
+                customFileReader.getCostList()
+        );
+
+        u = rnaAcidTranslator.translateRnaToAcid(customFileReader.getSequence1());
+        w = rnaAcidTranslator.translateRnaToAcid(customFileReader.getSequence2());
+        sq = new NeedlemanWunsch(u, w);
+        sq2 = new SmithWaterman(u, w);
+
+
+        System.out.printf("Dwie sekwencje po zamianie na aminokwasy: [%s] oraz [%s]%n", u, w);
+
+        System.out.println("Najlepsze zestawienie globalne:");
+        globalAlignments = sq.getAlignments();
+        System.out.println(Arrays.toString(globalAlignments));
+        String s = rnaAcidTranslator.translateAcidToRna(globalAlignments[0]);
+        String s1 = rnaAcidTranslator.translateAcidToRna(globalAlignments[1]);
+        System.out.printf("[%s, %s]%n", s, s1);
+        System.out.println("Dystans: " + costHandler.getAlignmentDistance(globalAlignments));
+        System.out.println("Podobieństwo: " + costHandler.getAlignmentSimilarity(globalAlignments));
+
+        System.out.println("Najlepsze zestawienie lokalne:");
+        localAlignments = sq2.getAlignments();
+        System.out.println(Arrays.toString(localAlignments));
+        s = rnaAcidTranslator.translateAcidToRna(localAlignments[0]);
+        s1 = rnaAcidTranslator.translateAcidToRna(localAlignments[1]);
+//        System.out.printf("[%s, %s]%n", s, s1);
+        System.out.println("Dystans: " + costHandler.getAlignmentDistance(localAlignments));
+        System.out.println("Podobieństwo: " + costHandler.getAlignmentSimilarity(localAlignments));
+        System.out.println("---------------------------------------------------------");
     }
 }
